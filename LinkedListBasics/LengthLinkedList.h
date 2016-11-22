b8a7587f720620bcbe8d34b83ad52fc25406d2e5
Safwan Ahmad <safwan.pico@gmail.com>
2016-11-22 22:58:31 +0530

    Moved to another directory

--- LinkedListBasics/LengthLinkedList.h
+++
@@ -1,9 +0,0 @@
-#ifndef LENGTHLINKEDLIST_H
-#define LENGTHLINKEDLIST_H
-
-
-extern int LengthLinkedList(struct LinkedList* header, const char parameter);
-static int FindLengthIterative(struct LinkedList *header);
-static int FindLengthRecursive(struct LinkedList *header);
-
-#endif
